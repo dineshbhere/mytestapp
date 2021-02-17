a7e823b28f851870a0ef73ea5acd21d18a54fd6c
Dinesh Bhere <m1055207@mindtree.com>
2021-02-17 11:19:03 +0530

    iamhere

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
+
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+
+var server;
+var port = process.env.PORT;
+server.listen(port, function() {
+  console.log("App is running on port " + port);
+});
 if (environment.production) {
   enableProdMode();
 }
